0d4027d92a1e02e856295876e8149c3c0790b19c
Cezar Halmagean <cezar@mixandgo.com>
2024-04-26 15:05:38 +0300

    Fix broken specs

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Guest, type: :model do
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
-  it { is_expected.to belong_to(:reservation).optional }
-  it { is_expected.to have_many(:reservations) }
+  it { is_expected.to have_and_belong_to_many(:reservations) }
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,5 +6,5 @@ RSpec.describe Reservation, type: :model do
   it { is_expected.to validate_presence_of(:room_type) }
   it { is_expected.to validate_presence_of(:quantity) }
   it { is_expected.to belong_to(:property) }
-  it { is_expected.to have_many(:guests) }
+  it { is_expected.to have_and_belong_to_many(:guests) }
 end
